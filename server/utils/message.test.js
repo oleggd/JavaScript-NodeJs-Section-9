0ea751a47c45ff38d904c895337124ebd7cc4cb8
oleg <olegg.dov@gmail.com>
2019-01-10 20:31:29 +0200

    JavaScript  NodeJs [Section 9] - Adding geolocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var { generateMessage } = require('./message');
+var { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -9,6 +9,21 @@ describe('generateMessage', () => {
         var response = generateMessage(from, text);
 
         expect(response).toMatchObject({from, text});
+        expect(typeof response.createdAt).toBe('number');
+    })
+});
+
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location message object', () => {
+        var from = 'test';
+        var latitude = 10;
+        var longitude = 11;
+        var url = 'https://www.google.com/maps?q=10,11';
+
+        var response = generateLocationMessage(from, latitude, longitude);
+
+        expect(response).toMatchObject({from, url});
         expect(typeof response.createdAt).toBe('number');
     })
 });
