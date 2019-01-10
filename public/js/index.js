e5f8128f26c231ed85e5a6ec44c1475c479161e0
oleg <olegg.dov@gmail.com>
2019-01-10 15:31:30 +0200

    JavaScript  NodeJs [Section 9] - Adding form for messages and show incomming messages

--- public/js/index.js
+++ public/js/index.js
@@ -8,6 +8,19 @@ socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(newMessage) {
+socket.on('newMessage', function (newMessage) {
     console.log('New message : ', newMessage);
+    var li = jQuery('<li></li>');
+    li.text(`${newMessage.from} : ${newMessage.text}`);
+    jQuery('#messages').append(li);
 });
+
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+    socket.emit('createMessage',{
+        from : 'User',
+        text : jQuery('input[name=message]').val()
+    }, function () {
+
+    });
+ });
