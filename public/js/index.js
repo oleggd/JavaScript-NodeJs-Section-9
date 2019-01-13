e13c4433802cdc77f099ff7b24cdd181a24f0bd5
oleg <olegg.dov@gmail.com>
2019-01-13 17:07:52 +0200

    JavaScript  NodeJs [Section 9] - Adding moment formatting

--- public/js/index.js
+++ public/js/index.js
@@ -8,18 +8,21 @@ socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function (newMessage) {
-    console.log('New message : ', newMessage);
+socket.on('newMessage', function (message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+
+    console.log('New message : ', message);
     var li = jQuery('<li></li>');
-    li.text(`${newMessage.from} : ${newMessage.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
     jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function (message) {
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
+    var formattedTime = moment(message.createdAt).format('h:mm a');
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
