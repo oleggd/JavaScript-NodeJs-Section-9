3bc8ee08e9c3f50849c268e60914dcf56b528914
oleg <olegg.dov@gmail.com>
2019-01-14 18:11:13 +0200

    JavaScript  NodeJs [Section 9] - Sending messages in same room

--- server/server.js
+++ server/server.js
@@ -36,13 +36,20 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (newMessage, callback) => {
-        console.log('createMessage', newMessage);
-        io.emit('newMessage', generateMessage( newMessage.from, newMessage.text));
+        var user = users.getUser(socket.id);
+
+        if (user && isRealString(newMessage.text)) {
+            io.to(user.room).emit('newMessage', generateMessage( user.name, newMessage.text));
+        }
         callback();
     });
 
     socket.on('createLocationMessage',(coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin',coords.latitude, coords.longitude));
+        var user = users.getUser(socket.id);
+        console.log('user : ', user);
+        if (user) {
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name,coords.latitude, coords.longitude));
+        }
     });
     socket.on('disconnect', () => {
         //console.log('User was disconnected from server');
